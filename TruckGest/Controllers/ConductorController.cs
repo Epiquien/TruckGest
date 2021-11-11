5e870ff1e45d46b629182a790f2f40483cb49121
Epiquien1 <rikiepiquien@gmail.com>
2021-11-11 07:15:25 -0500

    cambios reporte

--- TruckGest/Controllers/ConductorController.cs
+++ TruckGest/Controllers/ConductorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TruckGest.BaseDatos;
+using TruckGest.Models;
 
 namespace TruckGest.Controllers
 {
@@ -20,12 +21,37 @@ namespace TruckGest.Controllers
         }
         public ActionResult Reportes()
         {
-            return View();
+          
+            return View(conexionDB.reportes.ToList());
         }
         public ActionResult Camiones()
         {
 
             return View();
         }
+
+        public ActionResult addReporte(Reporte reporte )
+        {
+            if(Session["TypeUser"].ToString() == "2")
+            {
+                 
+                conexionDB.SaveChanges();
+                int id = Convert.ToInt32(Session["idUser"].ToString());
+
+                //  int id = Convert.ToInt32(Session["idUser"].ToString());
+                // var user_id = conexionDB.usuarios.Where(o => o.userName == newUser.userName).Select(o => o.id_usuario).First();
+                var conductor_id = conexionDB.conductores.Where(o => o.id_usuario == id).Select(o => o.id_conductor).First();
+
+                conexionDB.reportes.Add(reporte);
+                reporte.id_conductor = conductor_id;
+               
+                conexionDB.SaveChanges();
+                return RedirectToAction("Reportes");
+            }
+            return RedirectToAction("Index", "LogIn");
+        }
+
+            
+
+        }
     }
-}
